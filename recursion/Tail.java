d0e01a0e51bd60133dcb90fcddefce053ed445b3
dilara-jayatunga <thirandijayatunga@gmail.com>
2025-02-26 06:25:42 +0530

    tail recursion

---
+++ recursion/Tail.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class Tail {
+  public static void print(int a)
+{
+    if (a < 1) return;
+    else
+    {
+        System.out.println(a);
+
+        print(a / 2);
+    }
+}
+    public static void main(String[] args) {
+    
+        Tail obj = new Tail(); 
+        obj.print(10);  
+    }
+}
